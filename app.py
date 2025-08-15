0dc544ef907f1ada7e18709d139f8489eaff4d1a
hgastere <76585469+hgastere@users.noreply.github.com>
2025-08-15 15:24:14 +0200

    Update app.py
    removed /data/ stuff

--- app.py
+++ app.py
@@ -32,21 +32,6 @@ def new_color(new_color):
         return "Color not supported", 400
     return render_template('hello.html', name=socket.gethostname(), color=color_codes[new_color])
 
-@app.route('/read_file')
-def read_file():
-    try:
-        with open("/data/testfile.txt") as f:
-            contents = f.read()
-    except Exception as e:
-        return f"Error reading file: {e}", 500
-    return render_template('hello.html', name=socket.gethostname(), contents=contents, color=color_codes[color])
-    
-#@app.route('/read_file')
-#def read_file():
-#    f = open("/data/testfile.txt")
-#    contents = f.read()
-#    return render_template('hello.html', name=socket.gethostname(), contents=contents, color=color_codes[color])
-
 if __name__ == "__main__":
-#    app.run(host="0.0.0.0", port="8080")
-    app.run(host="0.0.0.0", port="8080", debug=True)
+    app.run(host="0.0.0.0", port="8080")
+#   app.run(host="0.0.0.0", port="8080", debug=True)
