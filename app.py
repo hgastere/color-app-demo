8a7aa5a2530f16e28b7ee5a51a3094bebaefa90e
hgastere <76585469+hgastere@users.noreply.github.com>
2025-08-15 15:27:41 +0200

    Update app.py
    debug true

--- app.py
+++ app.py
@@ -33,5 +33,5 @@ def new_color(new_color):
     return render_template('hello.html', name=socket.gethostname(), color=color_codes[new_color])
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="8080")
-#   app.run(host="0.0.0.0", port="8080", debug=True)
+#   app.run(host="0.0.0.0", port="8080")
+    app.run(host="0.0.0.0", port="8080", debug=True)
