74b504d55e7e8f0710e0b12a03665a5d68c13c84
hgastere <76585469+hgastere@users.noreply.github.com>
2025-08-15 15:18:17 +0200

    Update app.py
    troubleshooting /data/testfile.txt

--- app.py
+++ app.py
@@ -34,9 +34,18 @@ def new_color(new_color):
 
 @app.route('/read_file')
 def read_file():
-    f = open("/data/testfile.txt")
-    contents = f.read()
+    try:
+        with open("/data/testfile.txt") as f:
+            contents = f.read()
+    except Exception as e:
+        return f"Error reading file: {e}", 500
     return render_template('hello.html', name=socket.gethostname(), contents=contents, color=color_codes[color])
+    
+#@app.route('/read_file')
+#def read_file():
+#    f = open("/data/testfile.txt")
+#    contents = f.read()
+#    return render_template('hello.html', name=socket.gethostname(), contents=contents, color=color_codes[color])
 
 if __name__ == "__main__":
 #    app.run(host="0.0.0.0", port="8080")
